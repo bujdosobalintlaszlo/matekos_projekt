e3ca259014464b3746ca4af40aef623afc329929
Balint <118969351+bujdosobalintlaszlo@users.noreply.github.com>
2024-03-04 12:27:56 +0100

    Add files via upload

---
+++ kozgaz_matek_teszt.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2024 at 12:27 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kozgaz_matek_teszt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feladatesfeladatsorkapcs`
+--
+
+CREATE TABLE `feladatesfeladatsorkapcs` (
+  `feladatId` int(11) NOT NULL,
+  `feladatsorId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feladatesfelhasznalokapcs`
+--
+
+CREATE TABLE `feladatesfelhasznalokapcs` (
+  `feladatsorId` int(11) NOT NULL,
+  `felhasznaloId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feladatok`
+--
+
+CREATE TABLE `feladatok` (
+  `id` int(11) NOT NULL,
+  `temakor` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
+  `megoldas` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
+  `nehezseg` int(11) NOT NULL,
+  `pontszam` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feladatsor`
+--
+
+CREATE TABLE `feladatsor` (
+  `id` int(11) NOT NULL,
+  `idoKorlat` int(11) NOT NULL,
+  `dolgozatE` int(11) NOT NULL,
+  `tanarId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `felhaszesfeladkapcs`
+--
+
+CREATE TABLE `felhaszesfeladkapcs` (
+  `felhasznaloId` int(11) NOT NULL,
+  `feladatId` int(11) NOT NULL,
+  `megoldIdo` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `email` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
+  `jelszo` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
+  `felhasznaloNev` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `jog` int(11) NOT NULL,
+  `osztaly` varchar(3) COLLATE utf8_hungarian_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Dumping data for table `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`id`, `email`, `jelszo`, `felhasznaloNev`, `jog`, `osztaly`) VALUES
+(1, 'balint@gmail.com', 'ef28a6f147d822cdc65df689b118390d8245064e79d889386436bad6db42f9fd', 'balint89', 0, '12C');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `feladatesfeladatsorkapcs`
+--
+ALTER TABLE `feladatesfeladatsorkapcs`
+  ADD PRIMARY KEY (`feladatId`,`feladatsorId`),
+  ADD KEY `feladatsorId` (`feladatsorId`);
+
+--
+-- Indexes for table `feladatesfelhasznalokapcs`
+--
+ALTER TABLE `feladatesfelhasznalokapcs`
+  ADD PRIMARY KEY (`feladatsorId`,`felhasznaloId`),
+  ADD KEY `felhasznaloId` (`felhasznaloId`);
+
+--
+-- Indexes for table `feladatok`
+--
+ALTER TABLE `feladatok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feladatsor`
+--
+ALTER TABLE `feladatsor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `felhaszesfeladkapcs`
+--
+ALTER TABLE `felhaszesfeladkapcs`
+  ADD PRIMARY KEY (`felhasznaloId`,`feladatId`),
+  ADD KEY `feladatId` (`feladatId`);
+
+--
+-- Indexes for table `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `feladatok`
+--
+ALTER TABLE `feladatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feladatsor`
+--
+ALTER TABLE `feladatsor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feladatesfeladatsorkapcs`
+--
+ALTER TABLE `feladatesfeladatsorkapcs`
+  ADD CONSTRAINT `feladatesfeladatsorkapcs_ibfk_1` FOREIGN KEY (`feladatId`) REFERENCES `feladatok` (`id`),
+  ADD CONSTRAINT `feladatesfeladatsorkapcs_ibfk_2` FOREIGN KEY (`feladatsorId`) REFERENCES `feladatsor` (`id`);
+
+--
+-- Constraints for table `feladatesfelhasznalokapcs`
+--
+ALTER TABLE `feladatesfelhasznalokapcs`
+  ADD CONSTRAINT `feladatesfelhasznalokapcs_ibfk_1` FOREIGN KEY (`feladatsorId`) REFERENCES `feladatsor` (`id`),
+  ADD CONSTRAINT `feladatesfelhasznalokapcs_ibfk_2` FOREIGN KEY (`felhasznaloId`) REFERENCES `felhasznalok` (`id`);
+
+--
+-- Constraints for table `felhaszesfeladkapcs`
+--
+ALTER TABLE `felhaszesfeladkapcs`
+  ADD CONSTRAINT `felhaszesfeladkapcs_ibfk_1` FOREIGN KEY (`felhasznaloId`) REFERENCES `felhasznalok` (`id`),
+  ADD CONSTRAINT `felhaszesfeladkapcs_ibfk_2` FOREIGN KEY (`feladatId`) REFERENCES `feladatok` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
